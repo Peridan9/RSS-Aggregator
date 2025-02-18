26c55804416bca20befb1081f72ee9dbdb021c59
Peridan9 <Danielperi9@gmail.com>
2025-02-18 17:29:37 +0200

    Added a new command "handler_user" and added a commands struct with a method to run and register new commands

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 	"github.com/peridan9/RSS-Aggregator/internal/config" // Importing the config package to handle configuration operations.
 )
 
+type state struct {
+	cfg *config.Config
+}
+
 func main() {
 
 	// Read the existing configuration from the JSON file.
