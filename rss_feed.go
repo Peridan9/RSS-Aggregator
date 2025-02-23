3ec428fb68ea9031fbb568f3ff92720c37f84cef
Peridan9 <Danielperi9@gmail.com>
2025-02-23 17:12:06 +0200

    ADDED rss_feed file with structe and fetchfeed method

---
+++ rss_feed.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"encoding/xml"
+	"html"
+	"io"
+	"net/http"
+	"time"
+)
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+
+	httpClient := http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", "gator")
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var rssfeed RSSFeed
+	err = xml.Unmarshal(data, &rssfeed)
+	if err != nil {
+		return nil, err
+	}
+
+	rssfeed.Channel.Title = html.UnescapeString(rssfeed.Channel.Title)
+	rssfeed.Channel.Description = html.UnescapeString(rssfeed.Channel.Description)
+	for i, item := range rssfeed.Channel.Item {
+		item.Title = html.UnescapeString(item.Title)
+		item.Description = html.UnescapeString(item.Description)
+		rssfeed.Channel.Item[i] = item
+	}
+
+	return &rssfeed, nil
+
+}
