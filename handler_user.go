04c93c08cbb780bc451468ac2168d582a7160c25
Peridan9 <Danielperi9@gmail.com>
2025-02-23 15:25:18 +0200

    ADDED a user command which print all users

--- handler_user.go
+++ handler_user.go
@@ -68,6 +68,21 @@ func handlerLogin(s *state, cmd command) error {
 
 }
 
+func handlerUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("failed to retrive all users: %w", err)
+	}
+	for _, user := range users {
+		if user.Name == s.cfg.CurrentUser {
+			fmt.Printf("* %s (current)\n", user.Name)
+			continue
+		}
+		fmt.Printf("* %s\n", user.Name)
+	}
+	return nil
+}
+
 func printUser(user database.User) {
 	fmt.Printf(" * ID:      %v\n", user.ID)
 	fmt.Printf(" * Name:    %v\n", user.Name)
