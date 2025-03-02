84003b0a2f6732ab6cd8786c4c59cc949c7dabb6
Peridan9 <Danielperi9@gmail.com>
2025-03-02 12:05:54 +0200

    ADDED functionality to aggregate feeds with configurable request intervals and track last fetched timestamps

--- handler_rss.go
+++ handler_rss.go
@@ -3,14 +3,54 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
+
+	"github.com/peridan9/RSS-Aggregator/internal/database"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
-	if err != nil {
-		return fmt.Errorf("failed fetching rss feed: %w", err)
+	if len(cmd.Args) < 1 || len(cmd.Args) > 2 {
+		return fmt.Errorf("usage: %v <time_between_reqs>", cmd.Name)
 	}
 
-	fmt.Printf("Feed: %+v\n", feed)
-	return nil
+	timeBetweenReqs, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("couldn't parse time duration: %w", err)
+	}
+
+	log.Printf("Starting aggregator with time between requests %v", timeBetweenReqs)
+
+	ticker := time.NewTicker(timeBetweenReqs)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) {
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		log.Printf("couldn't get next feed to fetch: %v", err)
+		return
+	}
+	log.Printf("fetching feed %s", feed.Url)
+	scrapeFeed(s.db, feed)
+}
+
+func scrapeFeed(db *database.Queries, feed database.Feed) {
+	_, err := db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		log.Printf("couldn't mark feed fetched: %v", err)
+		return
+	}
+
+	feedData, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		log.Printf("couldn't fetch feed: %v", err)
+		return
+	}
+	for _, item := range feedData.Channel.Item {
+		fmt.Printf("Found Post: %s\n", item.Title)
+	}
+	log.Printf("Feed %s collected, %v posts found", feed.Url, len(feedData.Channel.Item))
 }
